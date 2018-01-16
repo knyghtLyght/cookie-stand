e026898f1b64f2606000f70d883065c7c8286916
Kevin <kevnaf@gmail.com>
2018-01-15 17:49:59 -0800

    proof of concept and fist object done

--- sales/js/salesApp.js
+++ sales/js/salesApp.js
@@ -7,24 +7,52 @@ var startHour = 6;
 // Calculates random cookie count given (minimum customers, maximum customers, and average cookie per customer)
 function cookiePerCustForcast (min, max, acpc) {
   var randomCustNum = Math.floor(Math.random() * ((max + 1) - min) + min);
-  console.log(randomCustNum);
+  console.log('min: ' + min + ' max: ' + max + ' acpc: ' + acpc + ' rand: ' + randomCustNum);
   var cookiesPurchasedGuess = randomCustNum * acpc;
   return cookiesPurchasedGuess;
 }
 // Builds hours array
 function dayLength (startHour, hoursOpen) {
   var hoursArray = [];
-  for(var i = startHour; i < hoursOpen + startHour; i++) {
+  for(var i = startHour; i < (hoursOpen + startHour); i++) {
     if(i <= 11) {
       hoursArray.push(i + 'am');
     }
     else {
-      hoursArray.push(i + 'pm');
+      if(i === 12) {
+        hoursArray.push(i + 'pm');
+      } else {
+        hoursArray.push((i - 12) + 'pm');
+      }
     }
+    console.log(hoursArray[i - startHour]);
+    console.log(hoursArray);
   }
   return hoursArray;
 }
 // Formats the li's for display
 function displayFormatter (hour, cookiesSold) {
-  return hour + ': ' + cookiesSold + 'cookies sold.';
+  return hour + ': ' + Math.round(cookiesSold) + ' cookies sold.';
 }
+var firstAndPike = {
+  render: function() {
+    // Setup known store data
+    var storeMinCust = 23;
+    var storeMaxCust = 65;
+    var storeACPC = 6.3;
+    var storeDayLength = dayLength(startHour, hoursOpen);
+    // Access the ul from sales.html
+    var ulEl = document.getElementById('firstAndPikeUl');
+    for(var i = 0; i < storeDayLength.length; i++) {
+      // Create list item
+      var liEl = document.createElement('li');
+      // Give it content
+      console.log(i);
+      liEl.textContent = displayFormatter(storeDayLength[i], cookiePerCustForcast(storeMinCust, storeMaxCust, storeACPC));
+      // Appened to the ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+firstAndPike.render();
