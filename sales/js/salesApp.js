ef0306a1de34ee91a8de64f7e7f2625a62455056
Kevin <kevnaf@gmail.com>
2018-01-15 18:00:59 -0800

    Remaining stores done and pages loads correctly

--- sales/js/salesApp.js
+++ sales/js/salesApp.js
@@ -54,5 +54,90 @@ var firstAndPike = {
     }
   }
 };
+var seaTac = {
+  render: function() {
+    // Setup known store data
+    var storeMinCust = 3;
+    var storeMaxCust = 24;
+    var storeACPC = 1.2;
+    var storeDayLength = dayLength(startHour, hoursOpen);
+    // Access the ul from sales.html
+    var ulEl = document.getElementById('seaTacUl');
+    for(var i = 0; i < storeDayLength.length; i++) {
+      // Create list item
+      var liEl = document.createElement('li');
+      // Give it content
+      console.log(i);
+      liEl.textContent = displayFormatter(storeDayLength[i], cookiePerCustForcast(storeMinCust, storeMaxCust, storeACPC));
+      // Appened to the ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+var seattleCenter = {
+  render: function() {
+    // Setup known store data
+    var storeMinCust = 11;
+    var storeMaxCust = 38;
+    var storeACPC = 3.7;
+    var storeDayLength = dayLength(startHour, hoursOpen);
+    // Access the ul from sales.html
+    var ulEl = document.getElementById('seattleCenterUl');
+    for(var i = 0; i < storeDayLength.length; i++) {
+      // Create list item
+      var liEl = document.createElement('li');
+      // Give it content
+      console.log(i);
+      liEl.textContent = displayFormatter(storeDayLength[i], cookiePerCustForcast(storeMinCust, storeMaxCust, storeACPC));
+      // Appened to the ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+var capHill = {
+  render: function() {
+    // Setup known store data
+    var storeMinCust = 20;
+    var storeMaxCust = 38;
+    var storeACPC = 2.3;
+    var storeDayLength = dayLength(startHour, hoursOpen);
+    // Access the ul from sales.html
+    var ulEl = document.getElementById('capHillUl');
+    for(var i = 0; i < storeDayLength.length; i++) {
+      // Create list item
+      var liEl = document.createElement('li');
+      // Give it content
+      console.log(i);
+      liEl.textContent = displayFormatter(storeDayLength[i], cookiePerCustForcast(storeMinCust, storeMaxCust, storeACPC));
+      // Appened to the ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+var alkiBeach = {
+  render: function() {
+    // Setup known store data
+    var storeMinCust = 2;
+    var storeMaxCust = 16;
+    var storeACPC = 4.6;
+    var storeDayLength = dayLength(startHour, hoursOpen);
+    // Access the ul from sales.html
+    var ulEl = document.getElementById('alkiUl');
+    for(var i = 0; i < storeDayLength.length; i++) {
+      // Create list item
+      var liEl = document.createElement('li');
+      // Give it content
+      console.log(i);
+      liEl.textContent = displayFormatter(storeDayLength[i], cookiePerCustForcast(storeMinCust, storeMaxCust, storeACPC));
+      // Appened to the ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
 
+//Call objects
 firstAndPike.render();
+seaTac.render();
+seattleCenter.render();
+capHill.render();
+alkiBeach.render();
