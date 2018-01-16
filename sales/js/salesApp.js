2c3885ccd3a7c3e21992b015aed374bea4c90db4
Kevin <kevnaf@gmail.com>
2018-01-15 16:22:01 -0800

    Helper functions done. I think

--- sales/js/salesApp.js
+++ sales/js/salesApp.js
@@ -1,2 +1,30 @@
 'use strict';
-alert('JS is linked');
+
+//Variable declaration
+var hoursOpen = 15;
+var startHour = 6;
+
+// Calculates random cookie count given (minimum customers, maximum customers, and average cookie per customer)
+function cookiePerCustForcast (min, max, acpc) {
+  var randomCustNum = Math.floor(Math.random() * ((max + 1) - min) + min);
+  console.log(randomCustNum);
+  var cookiesPurchasedGuess = randomCustNum * acpc;
+  return cookiesPurchasedGuess;
+}
+// Builds hours array
+function dayLength (startHour, hoursOpen) {
+  var hoursArray = [];
+  for(var i = startHour; i < hoursOpen + startHour; i++) {
+    if(i <= 11) {
+      hoursArray.push(i + 'am');
+    }
+    else {
+      hoursArray.push(i + 'pm');
+    }
+  }
+  return hoursArray;
+}
+// Formats the li's for display
+function displayFormatter (hour, cookiesSold) {
+  return hour + ': ' + cookiesSold + 'cookies sold.';
+}
